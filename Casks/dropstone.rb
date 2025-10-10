8872accbe1d6923da75000118b98e9c54e7a715f
Santosh Arron <73644573+santosharron@users.noreply.github.com>
2025-10-10 18:35:34 +0530

    Update dropstone.rb

--- Casks/dropstone.rb
+++ Casks/dropstone.rb
@@ -1,5 +1,5 @@
 cask "dropstone" do
-  version "2.0.2"
+  version "2.0.21"
   sha256 :no_check
 
   url "https://github.com/blankline-org/dropstone-releases/releases/download/v#{version}/Dropstone-v#{version}-macOS.zip"
